a2c1d0c184cd250f257c83036a534aef8c7ecf76
Aparna1004 <aparnashekhar151@gmail.com>
2024-08-14 19:52:05 +0530

    final

--- src/Components/DashboardHeader.jsx
+++ src/Components/DashboardHeader.jsx
@@ -4,7 +4,7 @@ import SignOutButton from "../Components/SignOutButton";
 
 function DashboardHeader() {
   return (
-    <header className='p-4 sm:p-5 shadow-sm border-b flex justify-between items-center'>
+    <header className='shadow-sm border-b flex justify-between items-center'>
       <div className='flex items-center'>
         {/* Placeholder for additional content, like a logo or title */}
       </div>
